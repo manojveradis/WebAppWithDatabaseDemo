a56f37770ee565d8f52af4897aeace1fb117a260
Manoj <manoj@veradistech.com>
2018-11-25 17:05:49 +0530

    Added a demo table Employer

---
+++ WebApp.Database/dbo/Tables/Employer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employer] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [CompanyName]   NVARCHAR (MAX) NULL,
+    [Email]		 NVARCHAR (50)  NULL, 
+	[Phone]		 NVARCHAR (50)  NULL, 
+	[Address]	 NVARCHAR (50)  NULL, 
+    CONSTRAINT [PK_Employer] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
